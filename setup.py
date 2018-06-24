5e012da2b367c2487774211fc13ea19c77f18195
robotgentleman <domx@robotgentleman.com>
2018-06-24 18:33:43 +0200

    dglab2018 setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dglab2018',
+      version='0.1',
+      description='Data processing and analysis scripts supporting the PhD project by one Dominik Gotojuch',
+      url='https://github.com/robotgentleman/dglab2018',
+      author='Dominik Gotojuch',
+      author_email='dominik@gotojuch.com',
+      license='MIT',
+      packages=['dglag2018'],
+      zip_safe=False)
