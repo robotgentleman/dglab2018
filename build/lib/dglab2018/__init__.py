646ec66a331c22101d1eeef0d53daa1c751a53a1
robotgentleman <domx@robotgentleman.com>
2018-06-24 19:54:04 +0200

    Removed build files

--- build/lib/dglab2018/__init__.py
+++
@@ -1,6 +0,0 @@
-import google.datalab.bigquery as bq
-
-def execute_query(queryStr):
-  query = bq.Query(queryStr.format(dataset))
-  outputOptions = bq.QueryOutput.table(use_cache=False)
-  return query.execute(output_options=outputOptions).result()
