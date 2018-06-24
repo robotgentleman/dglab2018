06ffc130a1ae74cf05b0f621fa5545a16b13449d
robotgentleman <domx@robotgentleman.com>
2018-06-24 21:40:42 +0200

    0.0.7

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
-	  
-with open("README.md", "r") as fh:
-    long_description = fh.read()
 
 setuptools.setup(
     name="dglab2018",
-    version="0.0.6",
+    version="0.0.7",
     author="Dominik Gotojuch",
     author_email="dominik@gotojuch.com",
     description="Data processing and analysis scripts supporting the PhD project by one Dominik Gotojuch",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/robotgentleman/dglab2018",
     packages=setuptools.find_packages(),
     classifiers=(
