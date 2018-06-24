9486fbd68620292a524165382b6a97bf601058b6
robotgentleman <domx@robotgentleman.com>
2018-06-24 19:17:14 +0200

    Shared ops

--- dglab2018/__init__.py
+++ dglab2018/__init__.py
@@ -1,3 +1,6 @@
-def joke():
-    return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
+import google.datalab.bigquery as bq
+
+def execute_query(queryStr):
+  query = bq.Query(queryStr.format(dataset))
+  outputOptions = bq.QueryOutput.table(use_cache=False)
+  return query.execute(output_options=outputOptions).result()
